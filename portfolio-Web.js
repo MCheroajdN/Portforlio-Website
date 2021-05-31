35f4f210bc3cbfeb83960e22c90a40cc49f8b5bd
MChris173 <mfonchris273@yahoo.com>
2021-05-31 14:09:49 +0100

    Added image, style and javascript code

---
+++ portfolio-Web.js
@@ -0,0 +1,9 @@
+function newPicture() {
+
+    document.getElementById("image").src="images/aboutimg1.jpg";
+}
+
+function oldPicture() {
+
+    document.getElementById("image").src="images/aboutimg.jpg";
+}
